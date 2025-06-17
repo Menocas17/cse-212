4735ab8a1ae80833030f1217dbf7b8b3bdcc3eb9
Menocas17 <rodolfomenocal7@gmail.com>
2025-06-17 16:42:34 -0600

    Finishing the coding activity of week 6

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -16,6 +16,7 @@ public class BinarySearchTree : IEnumerable<int>
         {
             _root = newNode;
         }
+
         // If the list is not empty, then only head will be affected.
         else
         {
@@ -81,6 +82,11 @@ public class BinarySearchTree : IEnumerable<int>
     private void TraverseBackward(Node? node, List<int> values)
     {
         // TODO Problem 3
+        if (node is not null) {
+        TraverseBackward(node.Right, values);
+        values.Add(node.Data);
+        TraverseBackward(node.Left, values);
+        }
     }
 
     /// <summary>
--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,11 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data) // With this we avoid the duplicates
+        {
+            return;
+        }
+
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +39,19 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
